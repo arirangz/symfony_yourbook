7b2471524ee18f34cf0300d128e5b1b9aafbb974
Anthony Laplane <anthony.laplane@gmail.com>
2023-04-27 20:03:57 +0200

    Upload fichier livre back

--- src/Controller/Admin/LivreCrudController.php
+++ src/Controller/Admin/LivreCrudController.php
@@ -6,6 +6,9 @@ use App\Entity\Livre;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class LivreCrudController extends AbstractCrudController
 {
@@ -17,7 +20,16 @@ class LivreCrudController extends AbstractCrudController
     
     public function configureFields(string $pageName): iterable
     {
-        yield from parent::configureFields($pageName);
+        //yield from parent::configureFields($pageName);
+
+        yield TextField::new('titre');
+        yield TextField::new('isbn');
+        yield TextareaField::new('description');
+
+        yield BooleanField::new('archive');
+
+        yield TextareaField::new('imageFile')->setFormType(VichImageType::class);
+
         yield AssociationField::new('auteur');
         yield AssociationField::new('editeur');
         yield AssociationField::new('genres');
--- src/Entity/Livre.php
+++ src/Entity/Livre.php
@@ -7,8 +7,11 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: LivreRepository::class)]
+#[Vich\Uploadable]
 class Livre
 {
     #[ORM\Id]
@@ -39,6 +42,19 @@ class Livre
     #[ORM\ManyToMany(targetEntity: Genre::class)]
     private Collection $genres;
 
+
+    #[Vich\UploadableField(mapping: 'livres', fileNameProperty: 'imageName', size: 'imageSize')]
+    private ?File $imageFile = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?string $imageName = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $imageSize = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
+
     public function __construct()
     {
         $this->genres = new ArrayCollection();
@@ -46,7 +62,7 @@ class Livre
 
     public function __toString()
     {
-        return $this->getTitre().' - '.$this->getAuteur().' - '.$this->getEditeur();
+        return $this->getTitre() . ' - ' . $this->getAuteur() . ' - ' . $this->getEditeur();
     }
 
     public function getId(): ?int
@@ -146,6 +162,67 @@ class Livre
     public function removeGenre(Genre $genre): self
     {
         $this->genres->removeElement($genre);
+
+        return $this;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): self
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function setImageSize(?int $imageSize): self
+    {
+        $this->imageSize = $imageSize;
+
+        return $this;
+    }
+
+    public function getImageSize(): ?int
+    {
+        return $this->imageSize;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
 
         return $this;
     }
